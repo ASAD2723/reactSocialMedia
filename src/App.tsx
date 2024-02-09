6cbb9b9a9ebc25df5825147d7dac28082787d225
ASAD2723 <khanasad323@gmail.com>
2024-02-09 21:59:45 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import { Home } from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 
 const App = () => {
   return (
@@ -13,7 +14,9 @@ const App = () => {
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
