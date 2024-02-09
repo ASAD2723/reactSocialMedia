3a08d1e8d0aad6c7f975c820ed903ab1fcdf9382
ASAD2723 <khanasad323@gmail.com>
2024-02-09 21:53:33 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
 
 const App = () => {
-  return <h1 className="underline font-bold text-3xl ">Hellooo</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
